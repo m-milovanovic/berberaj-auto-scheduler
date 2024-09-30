32bd85997b7bffb616bd7aac6c2584e44cc0e083
Miša Stefanović <mikmikyt1011@gmail.com>
2024-09-30 11:10:11 +0200

    feat: booking other slots if booking declines

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Builder, By } = require('selenium-webdriver');
+const { Builder, By, until } = require('selenium-webdriver');
 require('chromedriver');
 
 const schedule = require('node-schedule');
@@ -20,6 +20,8 @@ const timeslots = config.timeSlots;
 const MAX_MINUTES_TO_WAIT = config.maxMinutesToWait;
 
 const LOCATION = config.berberajLocation;
+
+const BARBERSHOP_URL = `https://berberaj.rs/${LOCATION}`;
 
 let employees = {};
 
@@ -48,6 +50,10 @@ const getBarber = async (driver, id) => {
     return await driver.findElement(By.css(`div[data-employee-id-no="${id}"]`));
 };
 
+/**
+ * Automates the process of booking appointments on the Berberaj website.
+ * @param {import('selenium-webdriver').WebDriver} driver - The Selenium WebDriver instance.
+ */
 const bookAppointment = async (driver) => {
     await driver.navigate().refresh();
     // Time out after an hour
@@ -70,35 +76,114 @@ const bookAppointment = async (driver) => {
 
         for (let i = 0; i < timeslots.length; i++) {
             try {
+                console.log(`Checking ${timeslots[i]}`);
                 await el
                     .findElement(
                         By.xpath(`.//*[text()[contains(.,'${timeslots[i]}')]]`)
                     )
                     .click();
                 timeslot = timeslots[i];
-                break;
-            } catch (_) {}
+                console.log('It is available, booking...');
+                await clearAndEnter(
+                    await driver.findElement(By.id('email')),
+                    user.email
+                );
+                await clearAndEnter(
+                    await driver.findElement(By.id('name')),
+                    user.name
+                );
+                await clearAndEnter(
+                    await driver.findElement(By.id('phone')),
+                    user.phone
+                );
+                await driver.findElement(By.id('submit-btn')).click();
+
+
+                const alertElement = await driver.wait(until.elementLocated(By.className('alert')), 5000);
+
+                const alertText = await alertElement.getAttribute('innerHTML');
+                console.log('Booking response: ' + alertText);
+
+                const alertAttribute = (
+                    await alertElement.getAttribute('class')
+                ).split(' ');
+                if (alertAttribute.includes('alert-success')) {
+                    console.log('Appointment booked successfully');
+                    break;
+                } else {
+                    console.log('Appointment booking failed');
+                    timeslot = null;
+                }
+            } catch (err) {
+                timeslot = null;
+                switch (err.name) {
+                    case 'NoSuchElementError':
+                        console.log('Timeslot is not found or not available!');
+                        break;
+                    case 'StaleElementReferenceError':
+                        console.log('Element is not attached to the page document');
+                        break;
+                    case 'ElementClickInterceptedError':
+                        console.log('Element is not clickable at point');
+                        break;
+                    case 'ElementNotInteractableError':
+                        console.log('Element is not interactable');
+                        break;
+                    case 'TimeoutError':
+                        console.log('Element is not found');
+                        break;
+                    default:
+                        console.log(err);
+                }
+            } finally {
+                try {
+                    await driver.findElement(By.className('close-btn')).click();
+                    await sleep(250);
+                } catch (_) {}
+            }
         }
         if (timeslot) {
-            await driver.findElement(By.id('email')).sendKeys(user.email);
-            await driver.findElement(By.id('name')).sendKeys(user.name);
-            await driver.findElement(By.id('phone')).sendKeys(user.phone);
-            await driver.findElement(By.id('submit-btn')).click();
-            console.log(`Termin zakazan u ${timeslot}`);
+            console.log('imaga');
             break;
-        } else await new Promise((r) => setTimeout(r, 1000));
+        }
+    }
+};
+
+const sleep = async (ms) => new Promise((r) => setTimeout(r, 1000));
+const clearAndEnter = async (element, text) => {
+    await element.clear();
+    await element.sendKeys(text);
+};
+/**
+ * Accepts the cookies on the Berberaj website.
+ * @param {import('selenium-webdriver').WebDriver} driver - The Selenium WebDriver instance.
+ */
+const acceptCookies = async (driver) => {
+    try {
+        console.log('Waiting for cookies info box to appear...');
+        const cookieInfoElement = await driver.findElement(
+            By.id('cookie-info')
+        );
+        await driver.wait(until.elementIsVisible(cookieInfoElement), 20000);
+        console.log('Trying to accept cookies...');
+        await driver.findElement(By.id('closes-cookie-info')).click();
+        console.log('Accepted cookies');
+    } catch (error) {
+        console.log(error);
+        console.log('Unable to find accept cookies button');
     }
 };
 
 (async () => {
     const driver = await new Builder().forBrowser(config.browser).build();
-    await driver.get(`https://berberaj.rs/${LOCATION}`);
+    await driver.get(BARBERSHOP_URL);
     employees = await getEmployees(driver);
 
     barber.name = barber.name.toUpperCase();
     console.log(barber.name);
 
     let currentHour = new Date().getHours();
+    await acceptCookies(driver); // Add await here to ensure sleep is completed before continuing
 
     if (currentHour < 9 || currentHour > 20) {
         schedule.scheduleJob('9 * * *', async () => {
