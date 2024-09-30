663d9445f1a08ac1da555b750f85f0941eb18e71
Miša Stefanović <mikmikyt1011@gmail.com>
2024-09-30 09:07:27 +0200

    feat: getting barber by name, task scheduler, multiple locations

--- index.js
+++ index.js
@@ -1,63 +1,109 @@
-const { Builder, By } = require("selenium-webdriver");
-require("chromedriver");
+const { Builder, By } = require('selenium-webdriver');
+require('chromedriver');
 
-const barberId = "7"; // Id 7 je Sale
-const email = ""; // popuni email
-const name = ""; // popuni ime
-const phone = ""; // popuni telefon
+const schedule = require('node-schedule');
 
-const timeslots = ["15:00", "14:00"];
+const barber = {
+    name: 'Dragoljub',
+    id: '13', // fallback if name is not found
+};
+
+const user = {
+    email: 'your-email@gmail.com',
+    name: 'Your Name',
+    phone: 'Your Number',
+};
+
+const timeslots = ['16:00', '16:30', '15:30'];
 
 const MAX_MINUTES_TO_WAIT = 60;
 
-(async function execute() {
-  const driver = await new Builder().forBrowser("chrome").build();
-  let timeslot;
-  try {
-    await driver.get("https://berberaj.rs");
-    const el = await driver.findElement(
-      By.css(`div[data-employee-id-no="${barberId}"]`)
-    );
+const LOCATION = 'centar';
 
+let employees = {};
+
+const getEmployees = async (driver) => {
+    let employees = {};
+
+    try {
+        const employeeBoxes = await driver.findElements(
+            By.className('employee-box')
+        );
+        for (const box of employeeBoxes) {
+            const employeeNameElement = await box.findElement(
+                By.css('.employee_description_name')
+            );
+            const employeeName = await employeeNameElement.getText();
+            const employeeId = await box.getAttribute('data-employee-id-no');
+            employees[employeeName] = employeeId;
+            return employees;
+        }
+    } catch (error) {
+        console.log('Unable to find employee names');
+    }
+};
+
+const getBarber = async (driver, id) => {
+    return await driver.findElement(By.css(`div[data-employee-id-no="${id}"]`));
+};
+
+const bookAppointment = async (driver) => {
+    await driver.navigate().refresh();
     // Time out after an hour
     setTimeout(async () => {
-      console.log("Nije se pojavio nijedan termin");
-      await driver.quit();
-      process.exit();
+        console.log('Nije se pojavio nijedan termin');
+        await driver.quit();
+        process.exit();
     }, MAX_MINUTES_TO_WAIT * 60000);
 
+    let timeslot = null;
+    let el;
+
     while (true) {
-      // Wait until 'Nema slobodnih termina.' is gone
-      try {
-        await el.findElement(
-          By.xpath(`.//*[text()[contains(.,'Nema slobodnih termina.')]]`)
+        el = await getBarber(
+            driver,
+            employees.hasOwnProperty(barber.name)
+                ? employees[barber.name]
+                : barber.id
         );
-        await new Promise((r) => setTimeout(r, 500));
-      } catch (error) {
-        //NoSuchElement error is thrown => 'Nema slobodnih termina.' is gone => timeslots have appeared
+
         for (let i = 0; i < timeslots.length; i++) {
-          try {
-            await el
-              .findElement(
-                By.xpath(`.//*[text()[contains(.,'${timeslots[i]}')]]`)
-              )
-              .click();
-            timeslot = timeslots[i];
-            break;
-          } catch (_) {}
+            try {
+                await el
+                    .findElement(
+                        By.xpath(`.//*[text()[contains(.,'${timeslots[i]}')]]`)
+                    )
+                    .click();
+                timeslot = timeslots[i];
+                break;
+            } catch (_) {}
         }
         if (timeslot) {
-          await driver.findElement(By.id("email")).sendKeys(email);
-          await driver.findElement(By.id("name")).sendKeys(name);
-          await driver.findElement(By.id("phone")).sendKeys(phone);
-          await driver.findElement(By.id("submit-btn")).click();
-          console.log(`Termin zakazan u ${timeslot}`);
-        } else {
-          console.log("Svi izabrani termini su zauzeti");
-        }
-      }
+            await driver.findElement(By.id('email')).sendKeys(user.email);
+            await driver.findElement(By.id('name')).sendKeys(user.name);
+            await driver.findElement(By.id('phone')).sendKeys(user.phone);
+            await driver.findElement(By.id('submit-btn')).click();
+            console.log(`Termin zakazan u ${timeslot}`);
+            break;
+        } else await new Promise((r) => setTimeout(r, 1000));
     }
-  } finally {
-    await driver.quit();
-  }
+};
+
+(async () => {
+    const driver = await new Builder().forBrowser('chrome').build();
+    await driver.get(`https://berberaj.rs/${LOCATION}`);
+    employees = await getEmployees(driver);
+
+    barber.name = barber.name.toUpperCase();
+    console.log(barber.name);
+
+    let currentHour = new Date().getHours();
+
+    if (currentHour < 9 || currentHour > 20) {
+        schedule.scheduleJob('9 * * *', async () => {
+            console.log('hello njorld');
+            await bookAppointment(driver);
+        });
+        console.log('Scheduled job for 9am');
+    } else await bookAppointment(driver);
 })();
